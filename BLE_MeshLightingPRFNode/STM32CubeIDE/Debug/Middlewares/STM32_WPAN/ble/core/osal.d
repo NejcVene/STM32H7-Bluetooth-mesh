b7af67522a6334836db0058f384653a38c559aa0
Nejc Vene <nejcvene.nv@gmail.com>
2024-10-25 01:08:27 +0200

    Cleanup

--- BLE_MeshLightingPRFNode/STM32CubeIDE/Debug/Middlewares/STM32_WPAN/ble/core/osal.d
+++
@@ -1,4 +0,0 @@
-Middlewares/STM32_WPAN/ble/core/osal.o: \
- F:/Nejc/Projekti/BLEMeshHomeAutomation/BLE_MeshLightingPRFNode/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
- F:/Nejc/Projekti/BLEMeshHomeAutomation/BLE_MeshLightingPRFNode/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
-F:/Nejc/Projekti/BLEMeshHomeAutomation/BLE_MeshLightingPRFNode/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
